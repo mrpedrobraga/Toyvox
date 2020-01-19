ab98ff6a68f0e74723cda80fbf8adf3235fc6873
HenriPK <auralinkk@gmail.com>
2020-01-19 20:41:27 -0300

    Committing so bagel can check the CMake scaffolding.

--- tvxsrc/tester.cpp
+++ tvxsrc/tester.cpp
@@ -1,5 +1,5 @@
-#pragma once
 #include "tvxcore.h"
+#include <SDL.h>
 #include <stdio.h>
 
 Scene scene;
@@ -7,12 +7,19 @@ Object obj1;
 
 int main()
 {
+
+	SDL_Init(SDL_INIT_VIDEO);
+
 	scene = Scene("New Layout");
 	obj1 = Object("player", vec3(3, 4, 6));
 	scene.addObj(&obj1);
 	
+	printf("%s", scene.getTitle());
 	printf("%s", scene.objectAt(0)->getObjectTypeTag());
 
 	getchar();
+
+	SDL_Quit();
+
 	return 0;
 }
