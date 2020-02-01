431f3fe69e0a54be547883e518d3cedc9fd0897e
Galen Cochrane <a.day.0ld.bag3l@gmail.com>
2020-02-01 09:25:30 -0700

    Revert "fixed a few things that were breaking minigame, added SDL/GL debugging functions"
    This reverts commit 2a9df67567cb9519b6ce3a4d3ed5bc0125514833.

--- tvxsrc/display.h
+++ tvxsrc/display.h
@@ -1,74 +1,73 @@
 
 #include "tvxutil.h"
-#include "sdldebug.h"
 
 struct Display {
-	SDL_Window* window = nullptr;
-	SDL_GLContext glcontext = nullptr;
+	SDL_Window* window;
+	SDL_GLContext glcontext;
 	GLuint window_flags;
 	glm::ivec2 resolution = glm::ivec2();
+
+	void startGLContext()
+	{
+		glcontext = SDL_GL_CreateContext(window);
+	}
 
 	void draw()
 	{
 		glClearColor(1, 1, 1, 1);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(0.1f, 0.3f, 0.8f, 1.5f);
-		SDL_GL_SwapWindow(window);
 	}
 
-	// FIX APPLIED: use "default arguments" instead of making a ton of different constructors
-	explicit Display(
-				const char *title = "Toyvox Game",
-				glm::ivec2 res = glm::ivec2(640, 360),
-				GLuint flags = SDL_WINDOW_SHOWN)
-				: window_flags(flags), resolution(res) {
-		
-		SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
-		SDL_SetHint(SDL_HINT_FRAMEBUFFER_ACCELERATION, "1");
-		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
-			fprintf(stderr, "Failed to initialize SDL: %s\n", SDL_GetError());
-			exit(1);
+	Display ():
+		window_flags(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE), resolution(glm::ivec2(640, 360))
+		{
+			window = SDL_CreateWindow(
+						"Toyvox Game",                  // window title
+						SDL_WINDOWPOS_UNDEFINED,           // initial x position
+						SDL_WINDOWPOS_UNDEFINED,           // initial y position
+						resolution.x,                               // width, in pixels
+						resolution.y,                               // height, in pixels
+						window_flags                  // flags - see below
+			); startGLContext();
 		}
 
-		SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, CONTEXT_FLAGS);
-
-		window = SDL_CreateWindow(
-					title,                    // window title
-					SDL_WINDOWPOS_UNDEFINED,  // x (SDL_WINDOWPOS_UNDEFINED means "doesn't matter")
-					SDL_WINDOWPOS_UNDEFINED,  // y (SDL_WINDOWPOS_UNDEFINED means "doesn't matter")
-					resolution.x,             // width
-					resolution.y,             // height
-					SDL_WINDOW_OPENGL | flags // flags
-		);
-		if ( ! window) {
-			fprintf(stderr, "Failed to create SDL window: %s\n", SDL_GetError());
-			exit(1);
-		}
-		glcontext = SDL_GL_CreateContext(window);
-		if ( ! glcontext) {
-			fprintf(stderr, "GL Context Creation Failed: %s\n", SDL_GetError());
-			exit(9000);
+	Display (GLuint flags):
+		window_flags(SDL_WINDOW_OPENGL | flags), resolution(glm::ivec2(640, 360))
+		{
+			window = SDL_CreateWindow(
+						"Toyvox Game",                  // window title
+						SDL_WINDOWPOS_UNDEFINED,           // initial x position
+						SDL_WINDOWPOS_UNDEFINED,           // initial y position
+						resolution.x,                               // width, in pixels
+						resolution.y,                               // height, in pixels
+						window_flags                  // flags - see below
+			); startGLContext();
 		}
 
-		// Load OpenGL pointers and print info
-		printf("OpenGL loaded\n");
-		gladLoadGLLoader(SDL_GL_GetProcAddress);
-		printf("Vendor:          %s\n", glGetString(GL_VENDOR));
-		printf("Renderer:        %s\n", glGetString(GL_RENDERER));
-		printf("Version OpenGL:  %s\n", glGetString(GL_VERSION));
-		printf("Version GLSL:    %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
-		
-		setupOpenglDebugCallback();
-		SDL_GL_SetSwapInterval(1);
-		glEnable(GL_DEPTH_TEST);
-		glEnable(GL_CULL_FACE);
-	}
+	Display (string& title, GLuint flags):
+		window_flags(SDL_WINDOW_OPENGL | flags), resolution(glm::ivec2(640, 360))
+		{
+			window = SDL_CreateWindow(
+						title.c_str(),                  // window title
+						SDL_WINDOWPOS_UNDEFINED,           // initial x position
+						SDL_WINDOWPOS_UNDEFINED,           // initial y position
+						resolution.x,                               // width, in pixels
+						resolution.y,                               // height, in pixels
+						window_flags                  // flags - see below
+			); startGLContext();
+		}
+
+	Display (string& title, glm::ivec2 res, GLuint flags):
+		window_flags(SDL_WINDOW_OPENGL | flags), resolution(res)
+		{
+			window = SDL_CreateWindow(
+						title.c_str(),                  // window title
+						SDL_WINDOWPOS_UNDEFINED,           // initial x position
+						SDL_WINDOWPOS_UNDEFINED,           // initial y position
+						resolution.x,                               // width, in pixels
+						resolution.y,                               // height, in pixels
+						window_flags                  // flags - see below
+			); startGLContext();
+		}
 
 	~Display()
 		{
--- tvxsrc/game.h
+++ tvxsrc/game.h
@@ -19,11 +19,12 @@ namespace tvx {
 		private:
 
 			std::shared_ptr<Scene> current_scene;
-			std::string game_title = "Game";
+			std::string game_title = strdup("Game");
 			glm::ivec2 resolution;
-			std::shared_ptr<Display> display; // FIX APPLIED: ptr to avoid accidental (de-)constructor calls
+			Display display;
 			bool should_stop = false;
-			
+
+			SDL_Event event = {};
 		public:
 			Scene &get_current_scene() const { return *current_scene; }
 			void set_current_scene(const std::shared_ptr<Scene> &new_scene) {
@@ -34,22 +35,24 @@ namespace tvx {
 				}
 			}
 
-			Display const &get_current_display() { return *display; }
-			void set_current_display(const std::shared_ptr<Display> &new_display) {
+			Display &get_current_display() { return display; }
+			void set_current_display(const Display &new_display) {
 				display = new_display;
 			}
-			
+
+
 			std::string get_title() const { return game_title; }
 			void set_title(std::string title) { game_title = std::move(title); }
 
-			glm::ivec2 get_resolution() const { return display->resolution; }
-			void set_resolution(glm::ivec2 res) { resolution = res; display->resolution = res; }
-			void set_resolution(int x, int y) { resolution = glm::ivec2(x, y); display->resolution = glm::ivec2(x, y); }
+			glm::ivec2 get_resolution() const { return display.resolution; }
+			void set_resolution(glm::ivec2 res) { resolution = res; display.resolution = res; }
+			void set_resolution(int x, int y) { resolution = glm::ivec2(x, y); display.resolution = glm::ivec2(x, y); }
 
 			Game():
 				resolution(640, 360)
 			{
-				display = std::make_shared<Display>(game_title.c_str(), resolution);
+				display = Display(game_title, resolution, SDL_WINDOW_RESIZABLE);
+				SDL_Init(SDL_INIT_EVERYTHING);	
 			}
 
 			~Game() {
@@ -60,14 +63,11 @@ namespace tvx {
 			void run() {
 				
 				while (!should_stop) { // main loop
-
-					if (display) { display->draw(); }
 					
 					if (!current_scene) {
 						return;
 					}
 					
-					SDL_Event event;
 					while (SDL_PollEvent(&event)) { // poll each frame
 
 						switch (event.type) { //Handle events
