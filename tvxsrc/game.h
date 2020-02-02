365038a2d87b81265b9ea7b7ded0440dac9c9b72
HenriPK <auralinkk@gmail.com>
2020-02-01 23:31:28 -0300

    Updated Readme.md

--- tvxsrc/game.h
+++ tvxsrc/game.h
@@ -62,15 +62,43 @@ namespace tvx::game {
 		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
 	}
 
+
+	GLuint VertexArrayID;
+	GLuint vertexbuffer;
+
+	void drawQuad() {
+
+		// 1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+		   0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		   3,                  // size
+		   GL_FLOAT,           // type
+		   GL_FALSE,           // normalized?
+		   0,                  // stride
+		   (void*)0            // array buffer offset
+		);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
+	}
+
 	void draw()
 	{
 
 		GLuint program = LoadShaders("native-extensions/iris-renderer/main.vert", "native-extensions/iris-renderer/main.frag");
 
+		glClearColor(0.1f, 0.3f, 0.8f, 1.5f);
+		glUseProgram(program);
+
 		while(!should_stop)
 		{
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			glClearColor(0.1f, 0.3f, 0.8f, 1.5f);
+
+			drawQuad();
+
 		  SDL_GL_SwapWindow(window);
 
 			if(current_scene->every_tick) current_scene->every_tick(0.0, *current_scene);
@@ -98,6 +126,21 @@ namespace tvx::game {
 	{
 		glcontext = SDL_GL_CreateContext(window);
 		gladLoadGLLoader(SDL_GL_GetProcAddress);
+
+		glGenVertexArrays(1, &VertexArrayID);
+		glBindVertexArray(VertexArrayID);
+
+		static const GLfloat g_vertex_buffer_data[] = {
+		   -1.0f, -1.0f, 0.0f,
+		   1.0f, -1.0f, 0.0f,
+		   0.0f,  1.0f, 0.0f,
+		};
+		// Generate 1 buffer, put the resulting identifier in vertexbuffer
+		glGenBuffers(1, &vertexbuffer);
+		// The following commands will talk about our 'vertexbuffer' buffer
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		// Give our vertices to OpenGL.
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
 		SDL_GL_SetSwapInterval(1);
 
--- tvxsrc/native-extensions/iris-renderer/main.vert
+++ tvxsrc/native-extensions/iris-renderer/main.vert
@@ -1,8 +1,8 @@
 #version 330 core
 
-layout (location=0) in vec3 vpos;
+layout (location=0) in vec3 model_vpos;
 
 void main()
 {
-  gl_Position = vec4(vpos, 1.0);
+  gl_Position = vec4(model_vpos, 1.0);
 }
