8f5e9387b2a4f1d72557e1c624b8b6108a7c03f4
HenriPK <auralinkk@gmail.com>
2020-02-02 19:36:00 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- tvxsrc/iris_test.cpp
+++ tvxsrc/iris_test.cpp
@@ -1,14 +1,67 @@
 #include <iostream>
+#include <Octree.hpp>
+#include "native-extensions/iris-renderer/iris-render.h"
 #include "tvxutil.h"
+#include "tvx_glutil.h"
 
 SDL_Window* window;
 SDL_GLContext glcontext;
 SDL_Event event;
 
+struct Color {
+private:
+	unsigned char mr, mg, mb, ma;
+public:
+
+	Color(unsigned char _r, unsigned char _g, unsigned char _b, unsigned char _a): mr(_r), mg(_g), mb(_b), ma(_a) {}
+
+	unsigned char& r() {return mr;}
+	unsigned char& g() {return mg;}
+	unsigned char& b() {return mb;}
+	unsigned char& a() {return ma;}
+
+	void set(unsigned char _r, unsigned char _g, unsigned char _b, unsigned char _a){mr = _r; mg = _g; mb = _b; ma = _a;}
+};
+Octree<Color> model(2048);
+
 bool quit = false;
 
-int main (int argc, char** argv) {
+#define ERRORCHECK while((err = glGetError()) != GL_NO_ERROR){ std::cout << "Error here"; }
+#define HEYTHERE std::cout << "Well this is supposed to be printed on the console."
+GLenum err;
 
+void generate_points(GLuint &AO, GLuint &BO) {
+	float vertices[] =
+	{
+			-0.5, -0.5, 0.0,
+			+0.5, -0.5, 0.0,
+			+0.5, +0.5, 0.0,
+			-0.5, +0.5, 0.0
+	};
+	glGenVertexArrays(1, &AO);
+	ERRORCHECK;
+	glGenBuffers(1, &BO);
+	ERRORCHECK;
+	glBindVertexArray(AO);
+	ERRORCHECK;
+	glBindBuffer(GL_ARRAY_BUFFER, BO);
+	ERRORCHECK;
+	{
+		glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), vertices, GL_STATIC_DRAW );
+		ERRORCHECK;
+		glEnableVertexAttribArray( 0 );
+		ERRORCHECK;
+		glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, nullptr );
+		ERRORCHECK;
+	}
+	glBindBuffer( GL_ARRAY_BUFFER, 0 );
+	ERRORCHECK;
+	glBindVertexArray( 0 );
+	ERRORCHECK;
+}
+
+int main (int argc, char** argv) {
+	HEYTHERE;
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_GL_LoadLibrary(NULL);
@@ -18,6 +71,9 @@ int main (int argc, char** argv) {
 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+
+	//Request Debug
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
 
 	window = SDL_CreateWindow("Toyvox Game",
 														SDL_WINDOWPOS_UNDEFINED,
@@ -30,13 +86,39 @@ int main (int argc, char** argv) {
 	gladLoadGLLoader(SDL_GL_GetProcAddress);
 
 	SDL_GL_SetSwapInterval(1);
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_CULL_FACE);
+
+	SDL_SetWindowGrab(window, SDL_TRUE);
+	SDL_SetRelativeMouseMode(SDL_TRUE);
+	HEYTHERE;
+	GLuint program = tvx::program_from_file("native-extensios/iris-renderer/main.vert", "native-extensions/iris-renderer/main.frag");
+	if (!program) { SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Couldn't compile shaders!!!\n"); }
+	ERRORCHECK;
+	glClearColor(0.2f, 0.4f, 0.8f, 1.0f);
+	GLuint AO, BO;
+	generate_points(AO, BO);
 
 	while (!quit) {
-	  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(0.1f, 0.3f, 0.8f, 1.5f);
-	  SDL_GL_SwapWindow(window);
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		ERRORCHECK;
+		glUseProgram(program);
+		ERRORCHECK;
+		glBindVertexArray(AO);
+		ERRORCHECK;
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		ERRORCHECK;
+		glBindVertexArray(0);
+		ERRORCHECK;
+	  glUseProgram(0);
+		ERRORCHECK;
+
+		SDL_GL_SwapWindow(window);
+
 		while(SDL_PollEvent(&event))
 		{
+
 			if(event.type == SDL_QUIT) quit = true;
 			if(event.type == SDL_KEYDOWN)
 				switch(event.key.keysym.sym) {
--- tvxsrc/tvx_glutil.h
+++ tvxsrc/tvx_glutil.h
@@ -1,91 +1,138 @@
 
 #include "tvxutil.h"
 
+static constexpr int MAX_SHADER_SZ = 65535;
 
-GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path){
+namespace tvx {
 
-	// Create the shaders
-	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+  float dynamic_normalize(float value, float min, float max)
+  {
+      return (value - min) / (max - min);
+  }
 
-	// Read the Vertex Shader code from the file
-	std::string VertexShaderCode;
-	std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
-	if(VertexShaderStream.is_open()){
-		std::stringstream sstr;
-		sstr << VertexShaderStream.rdbuf();
-		VertexShaderCode = sstr.str();
-		VertexShaderStream.close();
-	}else{
-		printf("Impossible to open %s. Are you in the right directory?\n", vertex_file_path);
-		getchar();
-		return 0;
+  glm::vec3 dynamic_normalize(glm::vec3 value, glm::vec3 min, glm::vec3 max)
+  {
+      return (value - min) / (max - min);
+  }
+
+	// TODO: For some reason, compilation errors are not being caught and instead bad shaders crash at runtime
+
+	GLuint program_from_string(const char *vsString, const char *fsString) {
+		if (!vsString || !fsString) {
+			SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Bad arguments passed to shaderLoadString");
+		}
+
+		GLuint shaderProgram = glCreateProgram();
+		GLuint vsHandle = glCreateShader(GL_VERTEX_SHADER);
+		GLuint fsHandle = glCreateShader(GL_FRAGMENT_SHADER);
+		{
+			glShaderSource(vsHandle, 1, &vsString, nullptr);
+			glCompileShader(vsHandle);
+			int params = -1;
+			glGetShaderiv(vsHandle, GL_COMPILE_STATUS, &params);
+			if (GL_TRUE != params) {
+				SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "vertex shader index %u did not compile\n", vsHandle);
+				const int maxLength = 2048;
+				int actualLength = 0;
+				char slog[2048];
+				glGetShaderInfoLog(vsHandle, maxLength, &actualLength, slog);
+				SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "shader info log for GL index %u:\n%s\n", vsHandle, slog);
+
+				glDeleteShader(vsHandle);
+				glDeleteShader(fsHandle);
+				glDeleteProgram(shaderProgram);
+				return 0;
+			}
+		}
+
+		{
+			glShaderSource(fsHandle, 1, &fsString, nullptr);
+			glCompileShader(fsHandle);
+			int params = -1;
+			glGetShaderiv(fsHandle, GL_COMPILE_STATUS, &params);
+			if (GL_TRUE != params) {
+				SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "fragment shader index %u did not compile\n", fsHandle);
+				const int maxLength = 2048;
+				int actualLength = 0;
+				char slog[2048];
+				glGetShaderInfoLog(fsHandle, maxLength, &actualLength, slog);
+				SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "shader info log for GL index %u:\n%s\n", fsHandle, slog);
+
+				glDeleteShader(vsHandle);
+				glDeleteShader(fsHandle);
+				glDeleteProgram(shaderProgram);
+				return 0;
+			}
+		}
+
+		glAttachShader(shaderProgram, fsHandle);
+		glAttachShader(shaderProgram, vsHandle);
+		{
+			glLinkProgram(shaderProgram);
+			glDeleteShader(vsHandle);
+			glDeleteShader(fsHandle);
+			int params = -1;
+			glGetProgramiv(shaderProgram, GL_LINK_STATUS, &params);
+			if (GL_TRUE != params) {
+				SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "could not link shader program GL index %u\n", shaderProgram);
+				const int maxLength = 2048;
+				int actualLength = 0;
+				char plog[2048];
+				glGetProgramInfoLog(shaderProgram, maxLength, &actualLength, plog);
+				SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "program info log for GL index %u:\n%s", shaderProgram, plog);
+
+				glDeleteProgram(shaderProgram);
+				return 0;
+			}
+		}
+		if ( ! shaderProgram) { SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to create shader!\n"); }
+		return shaderProgram;
 	}
 
-	// Read the Fragment Shader code from the file
-	std::string FragmentShaderCode;
-	std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
-	if(FragmentShaderStream.is_open()){
-		std::stringstream sstr;
-		sstr << FragmentShaderStream.rdbuf();
-		FragmentShaderCode = sstr.str();
-		FragmentShaderStream.close();
+	GLuint program_from_file(const char *vsFilename, const char *fsFilename) {
+		if (!vsFilename || !fsFilename) {
+			SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Bad arguments passed to shaderLoadFile");
+		}
+
+		char vsString[MAX_SHADER_SZ];
+		char fsString[MAX_SHADER_SZ];
+		vsString[0] = fsString[0] = '\0';
+		{
+			FILE *fp = fopen(("assets/shaders/" + std::string(vsFilename)).c_str(), "r");
+			if (!fp) {
+				SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Could not open vertex shader file `%s`\n", vsFilename);
+				return 0;
+			}
+			size_t count = fread(vsString, 1, MAX_SHADER_SZ - 1, fp);
+			if (count >= MAX_SHADER_SZ) { SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Shader size exceeded MAX_SHADER_SZ"); }
+			vsString[count] = '\0';
+			fclose(fp);
+		}
+		{
+			FILE *fp = fopen(("assets/shaders/" + std::string(fsFilename)).c_str(), "r");
+			if (!fp) {
+				SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Could not open fragment shader file `%s`\n", fsFilename);
+				return 0;
+			}
+			size_t count = fread(fsString, 1, MAX_SHADER_SZ - 1, fp);
+			if (count >= MAX_SHADER_SZ) { SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Shader size exceeded MAX_SHADER_SZ");}
+			fsString[count] = '\0';
+			fclose(fp);
+		}
+
+		return program_from_string(vsString, fsString);
 	}
 
-	GLint Result = GL_FALSE;
-	int InfoLogLength;
-
-	// Compile Vertex Shader
-	printf("Compiling shader : %s\n", vertex_file_path);
-	char const * VertexSourcePointer = VertexShaderCode.c_str();
-	glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
-	glCompileShader(VertexShaderID);
-
-	// Check Vertex Shader
-	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
-	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if ( InfoLogLength > 0 ){
-		std::vector<char> VertexShaderErrorMessage(InfoLogLength+1);
-		glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
-		printf("%s\n", &VertexShaderErrorMessage[0]);
+	void reload_program(GLuint *program, const char *vsFilename, const char *fsFilename) {
+		if (!program || !vsFilename || !fsFilename) {
+			SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Bad arguments passed to shaderReloadFile");
+		}
+		GLuint reloadedProgram = program_from_file(vsFilename, fsFilename);
+		if (program && reloadedProgram) {
+			glDeleteProgram(*program);
+			*program = reloadedProgram;
+		} else {
+			SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Shader reload failed!\n");
+		}
 	}
-
-	// Compile Fragment Shader
-	printf("Compiling shader : %s\n", fragment_file_path);
-	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
-	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
-	glCompileShader(FragmentShaderID);
-
-	// Check Fragment Shader
-	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
-	glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if ( InfoLogLength > 0 ){
-		std::vector<char> FragmentShaderErrorMessage(InfoLogLength+1);
-		glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
-		printf("%s\n", &FragmentShaderErrorMessage[0]);
-	}
-
-	// Link the program
-	printf("Linking program\n");
-	GLuint ProgramID = glCreateProgram();
-	glAttachShader(ProgramID, VertexShaderID);
-	glAttachShader(ProgramID, FragmentShaderID);
-	glLinkProgram(ProgramID);
-
-	// Check the program
-	glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
-	glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if ( InfoLogLength > 0 ){
-		std::vector<char> ProgramErrorMessage(InfoLogLength+1);
-		glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
-		printf("%s\n", &ProgramErrorMessage[0]);
-	}
-
-	glDetachShader(ProgramID, VertexShaderID);
-	glDetachShader(ProgramID, FragmentShaderID);
-
-	glDeleteShader(VertexShaderID);
-	glDeleteShader(FragmentShaderID);
-
-	return ProgramID;
 }
